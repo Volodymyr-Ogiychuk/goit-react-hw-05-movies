7cc8df4363603aa1ca4ed9b9f39d91a158cecddc
Volodymyr-Ogiychuk <volodymyr.ogiychuk@gmail.com>
2022-11-12 11:32:25 +0200

    Add changes to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ export const App = () => {
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'start',
         fontSize: 40,
         color: '#010101'
       }}
